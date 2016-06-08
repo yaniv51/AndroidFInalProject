1a219fa38fd61e9feecebde8271d8bac74bd93cf
Tal kramer <talkramer1@gmail.com>
2016-06-08 13:35:55 +0300

    working on gallery image view

---
+++ app/src/main/java/com/talkramer/finalproject/GridItem.java
@@ -0,0 +1,26 @@
+package com.talkramer.finalproject;
+
+public class GridItem {
+
+    private String image;
+
+    public GridItem() {
+        super();
+    }
+
+/*    public void setTitle(String title) {
+        this.title = title;
+    }*/
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+/*    public String getTitle() {
+        return title;
+    }*/
+
+    public String getImage() {
+        return image;
+    }
+}
