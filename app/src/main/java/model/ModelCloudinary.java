b694ac8332c92c1b2f2a6ae3828ef9ee658931dd
Yaniv Israel <yaniv.israel@gmail.com>
2016-07-01 15:49:12 +0300

    Initialize Cloudinary

---
+++ app/src/main/java/model/ModelCloudinary.java
@@ -0,0 +1,17 @@
+package model;
+
+import android.content.Context;
+//import com.cloudinary.Cloudinary;
+//import com.cloudinary.utils.ObjectUtils;
+
+/**
+ * Created by Yaniv on 01/07/16.
+ */
+public class ModelCloudinary {
+    //Cloudinary cloudinary;
+
+    public ModelCloudinary(Context context)
+    {
+
+    }
+}
