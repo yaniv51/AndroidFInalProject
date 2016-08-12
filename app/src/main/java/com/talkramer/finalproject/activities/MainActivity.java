ca004d5760b5c35dc48583b44268aea6807bb008
Tal kramer <talkramer1@gmail.com>
2016-08-12 15:29:12 +0300

    action bar

--- app/src/main/java/com/talkramer/finalproject/activities/MainActivity.java
+++ app/src/main/java/com/talkramer/finalproject/activities/MainActivity.java
@@ -1,12 +1,13 @@
 package com.talkramer.finalproject.activities;
 
-import android.app.Activity;
+
 import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.firebase.client.Firebase;
 import com.google.firebase.auth.FirebaseUser;
 import com.talkramer.finalproject.ApplicationStartup;
 import com.talkramer.finalproject.R;
@@ -14,7 +15,9 @@ import com.talkramer.finalproject.fragments.GridViewFragment;
 import com.talkramer.finalproject.fragments.SignInFragment;
 import com.talkramer.finalproject.model.Model;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ActionBarActivity {
+
+    private FragmentManager supportFragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,9 +55,7 @@ public class MainActivity extends Activity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_search_button, menu);
-        //getMenuInflater().inflate(R.menu.menu_home_button, menu);
-        //getMenuInflater().inflate(R.menu.menu_profile_button, menu);
+        getMenuInflater().inflate(R.menu.main, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
@@ -62,28 +63,26 @@ public class MainActivity extends Activity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.search: {
-//                NewStudentFragment newStudentFragment = new NewStudentFragment();
-//                FragmentManager fragmentManager = getSupportFragmentManager();
-//                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-//                fragmentTransaction.replace(R.id.main_frag_container, newStudentFragment, "newStudentFragment");
-//                fragmentTransaction.addToBackStack(null);
-//                fragmentTransaction.commit();
                 break;
             }
 
             case R.id.home: {
-//                FragmentManager fragmentManager = getSupportFragmentManager();
-//                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-//                Fragment page = getSupportFragmentManager().findFragmentByTag("studentDetailsFragment");
-//                page.onOptionsItemSelected(item);
+                GridViewFragment frag = new GridViewFragment();
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.add(R.id.main_frag_container, frag, "GridViewFragment");
+                transaction.commit();
                 break;
             }
 
             case R.id.profile: {
-//                FragmentManager fragmentManager = getSupportFragmentManager();
-//                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-//                Fragment page = getSupportFragmentManager().findFragmentByTag("studentDetailsFragment");
-//                page.onOptionsItemSelected(item);
+                break;
+            }
+
+            case R.id.logout: {
+                SignInFragment frag = new SignInFragment();
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.add(R.id.main_frag_container, frag, "signInFragment");
+                transaction.commit();
                 break;
             }
 
@@ -92,5 +91,4 @@ public class MainActivity extends Activity {
         }
         return  false;
     }
-
 }
