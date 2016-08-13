adbee976e09126478a612094e93a4180a0a10084
Tal kramer <talkramer1@gmail.com>
2016-08-13 20:09:56 +0300

    fix back from profile

--- app/src/main/java/com/talkramer/finalproject/fragments/GridViewFragment.java
+++ app/src/main/java/com/talkramer/finalproject/fragments/GridViewFragment.java
@@ -1,31 +1,28 @@
 package com.talkramer.finalproject.fragments;
 
 
+import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.app.Fragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
-import android.widget.Button;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.talkramer.finalproject.R;
+import com.talkramer.finalproject.model.Domain.Product;
+import com.talkramer.finalproject.model.Model;
+import com.talkramer.finalproject.model.Utils.Helper;
 
 import java.util.LinkedList;
 import java.util.List;
-import java.util.concurrent.ThreadFactory;
-
-import com.talkramer.finalproject.model.Utils.Helper;
-import com.talkramer.finalproject.model.Model;
-import com.talkramer.finalproject.model.Domain.Product;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -89,7 +86,7 @@ public class GridViewFragment extends Fragment {
 
                 // Replace whatever is in the fragment_container view with this fragment,
                 // and add the transaction to the back stack
-                transaction.replace(R.id.main_frag_container, newFragment);
+                transaction.add(R.id.main_frag_container, newFragment, "ProductDetailsFragment");
                 transaction.addToBackStack(null);
                 // Commit the transaction
                 transaction.commit();
